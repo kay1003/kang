539efec9f92c5350b62b51f1b29bdb744a1e61b8
changyikang <changyikang@egoonet.com>
2021-05-02 12:16:03 +0800

    feat: 搭建环境

--- src/main/java/com/kang/manage/ManageApplication.java
+++ src/main/java/com/kang/manage/ManageApplication.java
@@ -1,13 +1,28 @@
 package com.kang.manage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @company: manage
+ * @description: 管理后台启动类
+ * @author:
+ * @create:
+ **/
+@RestController
 @SpringBootApplication
+@MapperScan(value = {"com.kang.manage.domain", "com.kang.manage.mapper"})
 public class ManageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ManageApplication.class, args);
     }
 
+    @GetMapping("/who")
+    public String who(){
+        return "manage-server";
+    }
 }
